d487700baa670446369c32b9bb864cd727b9d004
suneet1212 <suneets2000@gmail.com>
2023-12-09 17:29:37 +0530

    deleting pyc files

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ options = coppeliasim.cmdopt.parse(args)
 appDir = os.path.dirname(args.coppeliasim_library)
 
 # Starts the simulation thread
-fastSimulation = True
+fastSimulation = False
 t = threading.Thread(target=simThreadFunc, args=(appDir, scenePath, fastSimulation))
 t.start()
 simRunGui(options) # Need to check how to run headless
